ca508f63880b2d6965f0032210d441ce0b830e8a
icyJoseph <sephxd1234@gmail.com>
2018-09-14 09:26:12 +0200

    feat: implement byAddress and byLatLng endpoints

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const app = express();
 const makeGeoURL = (key, encodedAddress) =>
   `http://www.mapquestapi.com/geocoding/v1/address?key=${key}&location=${encodedAddress}`;
 
+const makeReverseGeoURL = (key, { lat, lng }) =>
+  `http://www.mapquestapi.com/geocoding/v1/reverse?key=${key}&location=${lat},${lng}`;
+
 const makeWeatherURL = (key, { lat, lng }) =>
   `https://api.darksky.net/forecast/${key}/${lat},${lng}?units=si`;
 
@@ -30,11 +33,31 @@ const getWeather = (url, res, lat, lng, options = {}) => {
     });
 };
 
+const getGeoLocation = url => {
+  return axios.get(url).then(({ data }) => {
+    if (data.results === undefined || data.results[0].locations.length === 0) {
+      return res.status(400).send({ reason: "Your query gave no results" });
+    }
+
+    const [
+      { street, postalCode, adminArea5, adminArea1, latLng }
+    ] = data.results[0].locations;
+
+    return {
+      street,
+      postalCode,
+      city: adminArea5,
+      country: adminArea1,
+      ...latLng
+    };
+  });
+};
+
 const jsonParser = bodyParser.json();
 
 app.use(jsonParser);
 
-app.post("/geoweather", (req, res) => {
+app.post("/byAddress", (req, res) => {
   const { webtaskContext, body } = req;
   const { address } = body;
   const {
@@ -42,47 +65,31 @@ app.post("/geoweather", (req, res) => {
   } = webtaskContext;
 
   const encodedAddress = encodeURIComponent(address);
-  const url = makeGeoURL(GEOLOCATION_KEY, encodedAddress);
+  const geoURL = makeGeoURL(GEOLOCATION_KEY, encodedAddress);
 
-  return axios
-    .get(url)
-    .then(({ data }) => {
-      if (
-        data.results === undefined ||
-        data.results[0].locations.length === 0
-      ) {
-        return res.status(400).send({ reason: "Your query gave no results" });
-      }
-
-      const [
-        { street, postalCode, adminArea5, adminArea1, latLng }
-      ] = data.results[0].locations;
-
-      return {
-        street,
-        postalCode,
-        city: adminArea5,
-        country: adminArea1,
-        ...latLng
-      };
-    })
+  return getGeoLocation(geoURL)
     .then(({ lat, lng, ...rest }) => {
-      const url = makeWeatherURL(WEATHER_KEY, { lat, lng });
-      return getWeather(url, res, lat, lng, rest);
+      const weatherURL = makeWeatherURL(WEATHER_KEY, { lat, lng });
+      return getWeather(weatherURL, res, lat, lng, rest);
     })
     .catch(() => res.status(400).send({ reason: "Location failed" }));
 });
 
-app.post("/weather", (req, res) => {
+app.post("/byLatLng", (req, res) => {
   const { webtaskContext, body } = req;
   const {
-    secrets: { WEATHER_KEY }
+    secrets: { GEOLOCATION_KEY, WEATHER_KEY }
   } = webtaskContext;
 
   const { lat, lng } = body;
-  const url = makeWeatherURL(WEATHER_KEY, { lat, lng });
+  const geoURL = makeReverseGeoURL(GEOLOCATION_KEY, { lat, lng });
 
-  return getWeather(url, res, lat, lng);
+  // Later on move onto parallel requests
+  return getGeoLocation(geoURL).then(geolocation => {
+    const weatherURL = makeWeatherURL(WEATHER_KEY, { lat, lng });
+
+    return getWeather(weatherURL, res, lat, lng, geolocation);
+  });
 });
 
 module.exports = Webtask.fromExpress(app);
